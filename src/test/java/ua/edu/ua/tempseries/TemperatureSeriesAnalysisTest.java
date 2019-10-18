dfe71fee6e9771a66f2ac54a5badd66bc976797c
o-matskiv <47506194+o-matskiv@users.noreply.github.com>
2019-10-18 18:33:58 +0300

    Update TemperatureSeriesAnalysisTest.java

--- src/test/java/ua/edu/ua/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/edu/ua/tempseries/TemperatureSeriesAnalysisTest.java
@@ -1,66 +1,66 @@
-package ua.edu.ucu.tempseries;
+// package ua.edu.ucu.tempseries;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import org.junit.Ignore;
-import org.junit.Before;
-import java.util.InputMismatchException;
-import java.util.Arrays;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
-
-public class TemperatureSeriesAnalysisTest {
-    double[] temperatureSeries = {0.0, -1.0, 7, -10, -20.0, 60.0};
+// import static org.junit.Assert.*;
+// import org.junit.Test;
+// import org.junit.Ignore;
+// import org.junit.Before;
+// import java.util.InputMismatchException;
+// import java.util.Arrays;
+// import static org.junit.Assert.assertArrayEquals;
+// import static org.junit.Assert.assertEquals;
 
 
-  @Test
-   public void testAverage(){
-       expected = 6;
-       actual = temperatureSeries.average();
-       assertEquals(expected, actual, 0.000001);
-   }
-   @Test
-   public void testDeviation(){
-        expected = 6;
-        actual = temperatureSeries.average();
-        assertEquals(expected, actual, 0.000001);
-   }
-   @Test
-   public void testMin(){
-        expected = -20;
-        actual = temperatureSeries.min();
-        assertEquals(expected, actual, 0.000001);
-   }
-   @Test
-   public void testMax(){
-       expected = 60;
-       actual = temperatureSeries.max();
-       assertEquals(expected, actual, 0.000001);
-   }
-   @Test
-   public void testClosesttoZero(){
-       expected = 0;
-       actual = temperatureSeries.findTempClosestToZero();
-       assertEquals(expected, actual, 0.000001);
-   }
-   @Test
-   public void testClosesttoValue(){
-       expected = 7;
-       actual = temperatureSeries.findTempClosestToValue(6);
-       assertEquals(expected, actual, 0.000001);
-   }
-   @Test
-   public void findTempsLessThen(){
-        expected = {0.0, -1.0, -10, -20.0};
-        actual = temperatureSeries.findTempsLessThen(1);
-        assertArrayEquals(expected, actual);
-   }
+// public class TemperatureSeriesAnalysisTest {
+//     double[] temperatureSeries = {0.0, -1.0, 7, -10, -20.0, 60.0};
 
 
+//   @Test
+//    public void testAverage(){
+//        expected = 6;
+//        actual = temperatureSeries.average();
+//        assertEquals(expected, actual, 0.000001);
+//    }
+//    @Test
+//    public void testDeviation(){
+//         expected = 6;
+//         actual = temperatureSeries.average();
+//         assertEquals(expected, actual, 0.000001);
+//    }
+//    @Test
+//    public void testMin(){
+//         expected = -20;
+//         actual = temperatureSeries.min();
+//         assertEquals(expected, actual, 0.000001);
+//    }
+//    @Test
+//    public void testMax(){
+//        expected = 60;
+//        actual = temperatureSeries.max();
+//        assertEquals(expected, actual, 0.000001);
+//    }
+//    @Test
+//    public void testClosesttoZero(){
+//        expected = 0;
+//        actual = temperatureSeries.findTempClosestToZero();
+//        assertEquals(expected, actual, 0.000001);
+//    }
+//    @Test
+//    public void testClosesttoValue(){
+//        expected = 7;
+//        actual = temperatureSeries.findTempClosestToValue(6);
+//        assertEquals(expected, actual, 0.000001);
+//    }
+//    @Test
+//    public void findTempsLessThen(){
+//         expected = {0.0, -1.0, -10, -20.0};
+//         actual = temperatureSeries.findTempsLessThen(1);
+//         assertArrayEquals(expected, actual);
+//    }
 
 
 
 
 
-}
+
+
+// }
