bfa5999d258d08067b9f1fe6cde22cf80e232939
o-matskiv <47506194+o-matskiv@users.noreply.github.com>
2019-10-18 18:06:30 +0300

    Update TemperatureSeriesAnalysisTest.java

--- src/test/java/ua/edu/ua/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/edu/ua/tempseries/TemperatureSeriesAnalysisTest.java
@@ -20,7 +20,7 @@ public class TemperatureSeriesAnalysisTest {
         assertEquals(expResult, actualResult, 0.00001);
     }
 
-    @Ignore
+    
     @Test(expected = IllegalArgumentException.class)
     public void testAverageWithEmptyArray() {
         double[] temperatureSeries = {};
@@ -30,7 +30,7 @@ public class TemperatureSeriesAnalysisTest {
         seriesAnalysis.average();
     }
 
-    @Ignore
+    
     @Test
     public void testAverage() {
         double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
@@ -38,9 +38,9 @@ public class TemperatureSeriesAnalysisTest {
         double expResult = 1.0;
 
         double actualResult = seriesAnalysis.average();
-        
-        assertEquals(expResult, actualResult, 0.00001);        
+
+        assertEquals(expResult, actualResult, 0.00001);
     }
-    
+
 
 }
